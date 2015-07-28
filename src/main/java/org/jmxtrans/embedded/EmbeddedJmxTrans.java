4f1ce2ccce17189ec36aaded7f7c9162fdaf44e6
Cyrille Le Clerc <cyrille@cyrilleleclerc.com>
2015-07-28 17:16:34 +0200

    Fix thread safety during the "stop()" phase.

--- src/main/java/org/jmxtrans/embedded/EmbeddedJmxTrans.java
+++ src/main/java/org/jmxtrans/embedded/EmbeddedJmxTrans.java
@@ -41,23 +41,24 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
- *
- * <strong>JMX Queries</strong>
+ * <h2>JMX Queries</h2>
  *
  * If the JMX query returns several mbeans (thanks to '*' or '?' wildcards),
  * then the configured attributes are collected on all the returned mbeans.
  *
- *
- * <strong>Output Writers</strong>
+ * <h2>Output Writers</h2>
  *
  * {@linkplain OutputWriter}s can be defined at the query level or globally at the {@link EmbeddedJmxTrans} level.
  * The {@linkplain OutputWriter}s that are effective for a {@linkplain Query} are accessible
  * via {@link Query#getEffectiveOutputWriters()}
  *
  *
- * <strong>Collected Metrics / Query Results</strong>
+ * <h2>Collected Metrics / Query Results</h2>
  *
  * Default behavior is to store the query results at the query level (see {@linkplain Query#queryResults}) to resolve the
  * effective {@linkplain OutputWriter}s at result export time ({@linkplain org.jmxtrans.embedded.Query#getEffectiveOutputWriters()}).
@@ -74,16 +75,16 @@ import java.util.concurrent.atomic.AtomicBoolean;
  */
 public class EmbeddedJmxTrans implements EmbeddedJmxTransMBean {
 
-	public EmbeddedJmxTrans() {
-		super();
-	}
-	
-    public EmbeddedJmxTrans(MBeanServer mbeanServer) {
-		super();
-		this.mbeanServer = mbeanServer;
-	}
+    public EmbeddedJmxTrans() {
+        super();
+    }
 
-	/**
+    public EmbeddedJmxTrans(MBeanServer mbeanServer) {
+        super();
+        this.mbeanServer = mbeanServer;
+    }
+
+    /**
      * Shutdown hook to collect and export metrics a last time if {@link EmbeddedJmxTrans#stop()} was not called.
      */
     private class EmbeddedJmxTransShutdownHook extends Thread {
@@ -166,7 +167,11 @@ public class EmbeddedJmxTrans implements EmbeddedJmxTransMBean {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    private boolean running = false;
+    enum State {STOPPED, STARTING, STARTED, STOPPING}
+
+    private AtomicReference<State> state = new AtomicReference(State.STOPPED);
+
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
 
     private ScheduledExecutorService collectScheduledExecutor;
 
@@ -200,41 +205,80 @@ public class EmbeddedJmxTrans implements EmbeddedJmxTransMBean {
      */
     @PostConstruct
     public synchronized void start() throws Exception {
-        if(running) {
-            logger.debug("Ignore start() command for already running instance");
-            return;
-        }
+        readWriteLock.writeLock().lock();
+        try {
+            State state = this.state.get();
+            if (!State.STOPPED.equals(state)) {
+                logger.warn("Ignore start() command for {} instance", state);
+                return;
+            }
+            this.state.set(State.STARTING);
 
-        for (Query query : queries) {
-            query.start();
-        }
-        for (OutputWriter outputWriter : outputWriters) {
-            outputWriter.start();
-        }
+            for (Query query : queries) {
+                query.start();
+            }
+            for (OutputWriter outputWriter : outputWriters) {
+                outputWriter.start();
+            }
 
-        collectScheduledExecutor = Executors.newScheduledThreadPool(getNumQueryThreads(), new NamedThreadFactory("jmxtrans-collect-", true));
-        exportScheduledExecutor = Executors.newScheduledThreadPool(getNumExportThreads(), new NamedThreadFactory("jmxtrans-export-", true));
+            collectScheduledExecutor = Executors.newScheduledThreadPool(getNumQueryThreads(), new NamedThreadFactory("jmxtrans-collect-", true));
+            exportScheduledExecutor = Executors.newScheduledThreadPool(getNumExportThreads(), new NamedThreadFactory("jmxtrans-export-", true));
 
-        for (final Query query : getQueries()) {
-            collectScheduledExecutor.scheduleWithFixedDelay(new Runnable() {
-                @Override
-                public void run() {
-                    query.collectMetrics();
-                }
-            }, 0, getQueryIntervalInSeconds(), TimeUnit.SECONDS);
-            // start export just after first collect
-            exportScheduledExecutor.scheduleWithFixedDelay(new Runnable() {
-                @Override
-                public void run() {
-                    query.exportCollectedMetrics();
-                }
-            }, getQueryIntervalInSeconds() + 1, getExportIntervalInSeconds(), TimeUnit.SECONDS);
+            for (final Query query : getQueries()) {
+                collectScheduledExecutor.scheduleWithFixedDelay(new Runnable() {
+                    @Override
+                    public void run() {
+                        readWriteLock.readLock().lock();
+                        try {
+                            State state = EmbeddedJmxTrans.this.state.get();
+                            if (!State.STARTED.equals(state)) {
+                                logger.debug("Ignore query.collectMetrics() command for {} instance", state);
+                                return;
+                            }
+                            query.collectMetrics();
+                        } finally {
+                            readWriteLock.readLock().unlock();
+                        }
+                    }
+
+                    @Override
+                    public String toString() {
+                        return "Collector[" + query + "]";
+                    }
+                }, 0, getQueryIntervalInSeconds(), TimeUnit.SECONDS);
+
+                // start export just after first collect
+                exportScheduledExecutor.scheduleWithFixedDelay(new Runnable() {
+                    @Override
+                    public void run() {
+                        readWriteLock.readLock().lock();
+                        try {
+                            State state = EmbeddedJmxTrans.this.state.get();
+                            if (!State.STARTED.equals(state)) {
+                                logger.debug("Ignore query.exportCollectedMetrics() command for {} instance", state);
+                                return;
+                            }
+
+                            query.exportCollectedMetrics();
+                        } finally {
+                            readWriteLock.readLock().unlock();
+                        }
+                    }
+
+                    @Override
+                    public String toString() {
+                        return "Exporter[" + query + "]";
+                    }
+                }, getQueryIntervalInSeconds() + 1, getExportIntervalInSeconds(), TimeUnit.SECONDS);
+            }
+
+            shutdownHook = new EmbeddedJmxTransShutdownHook();
+            shutdownHook.registerToRuntime();
+            this.state.set(State.STARTED);
+            logger.info("EmbeddedJmxTrans started");
+        } finally {
+            readWriteLock.writeLock().unlock();
         }
-
-        shutdownHook = new EmbeddedJmxTransShutdownHook();
-        shutdownHook.registerToRuntime();
-        running = true;
-        logger.info("EmbeddedJmxTrans started");
     }
 
 
@@ -243,28 +287,43 @@ public class EmbeddedJmxTrans implements EmbeddedJmxTransMBean {
      */
     @PreDestroy
     public synchronized void stop() throws Exception {
-        if(!running) {
-            logger.debug("Ignore stop() command for not running instance");
-            return;
-        }
+        readWriteLock.writeLock().lock();
         try {
-            collectScheduledExecutor.shutdown();
-            try {
-                collectScheduledExecutor.awaitTermination(getQueryIntervalInSeconds(), TimeUnit.SECONDS);
-            } catch (InterruptedException e) {
-                logger.warn("Ignore InterruptedException stopping", e);
+            State state = this.state.get();
+            if (!State.STARTED.equals(state)) {
+                logger.debug("Ignore stop() command for " + state + " instance");
+                return;
             }
-            exportScheduledExecutor.shutdown();
+            this.state.set(State.STOPPING);
             try {
-                exportScheduledExecutor.awaitTermination(getExportIntervalInSeconds(), TimeUnit.SECONDS);
-            } catch (InterruptedException e) {
-                logger.warn("Ignore InterruptedException stopping", e);
+                collectScheduledExecutor.shutdown();
+                try {
+                    boolean terminated = collectScheduledExecutor.awaitTermination(getQueryIntervalInSeconds(), TimeUnit.SECONDS);
+                    if (!terminated) {
+                        List<Runnable> tasks = collectScheduledExecutor.shutdownNow();
+                        logger.warn("Collect executor could not shutdown in time. Abort tasks " + tasks);
+                    }
+                } catch (InterruptedException e) {
+                    logger.warn("Ignore InterruptedException stopping", e);
+                }
+                exportScheduledExecutor.shutdown();
+                try {
+                    boolean terminated = exportScheduledExecutor.awaitTermination(getExportIntervalInSeconds(), TimeUnit.SECONDS);
+                    if (!terminated) {
+                        List<Runnable> tasks = exportScheduledExecutor.shutdownNow();
+                        logger.warn("Export executor could not shutdown in time. Abort tasks " + tasks);
+                    }
+                } catch (InterruptedException e) {
+                    logger.warn("Ignore InterruptedException stopping", e);
+                }
+            } catch (RuntimeException e) {
+                logger.warn("Failure while shutting down ExecutorServices", e);
             }
-        } catch (RuntimeException e) {
-            logger.warn("Failure while shutting down ExecutorServices", e);
+            shutdownHook.onStop();
+            this.state.set(State.STOPPED);
+        } finally {
+            readWriteLock.writeLock().unlock();
         }
-        shutdownHook.onStop();
-        running = false;
     }
 
 
@@ -273,8 +332,18 @@ public class EmbeddedJmxTrans implements EmbeddedJmxTransMBean {
      */
     @Override
     public void collectMetrics() {
-        for (Query query : getQueries()) {
-            query.collectMetrics();
+        readWriteLock.readLock().lock();
+        try {
+            State state = this.state.get();
+            if (!State.STARTED.equals(state)) {
+                logger.debug("Ignore collectMetrics() command for " + state + " instance");
+                return;
+            }
+            for (Query query : getQueries()) {
+                query.collectMetrics();
+            }
+        } finally {
+            readWriteLock.readLock().unlock();
         }
     }
 
@@ -283,8 +352,18 @@ public class EmbeddedJmxTrans implements EmbeddedJmxTransMBean {
      */
     @Override
     public void exportCollectedMetrics() {
-        for (Query query : getQueries()) {
-            query.exportCollectedMetrics();
+        readWriteLock.readLock().lock();
+        try {
+            State state = this.state.get();
+            if (!State.STARTED.equals(state)) {
+                logger.debug("Ignore collectMetrics() command for not running instance");
+                return;
+            }
+            for (Query query : getQueries()) {
+                query.exportCollectedMetrics();
+            }
+        } finally {
+            readWriteLock.readLock().unlock();
         }
     }
 
@@ -301,7 +380,8 @@ public class EmbeddedJmxTrans implements EmbeddedJmxTransMBean {
     @Override
     public String toString() {
         return "EmbeddedJmxTrans{" +
-                " queries=" + queries +
+                "state=" + state +
+                ", queries=" + queries +
                 ", outputWriters=" + outputWriters +
                 ", numQueryThreads=" + numQueryThreads +
                 ", queryIntervalInSeconds=" + queryIntervalInSeconds +
